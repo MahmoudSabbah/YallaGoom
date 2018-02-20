71c5df3a25e1499ced717d3e83480abfd3d45181
MahmoudSabbah <mahmouds639@gmail.com>
2018-02-20 11:19:54 +0200

    add friends

--- app/src/main/java/com/yallagoom/fragment/eventTapFragment/FriendEventFragment.java
+++ app/src/main/java/com/yallagoom/fragment/eventTapFragment/FriendEventFragment.java
@@ -24,20 +24,8 @@ import com.yallagoom.interfaces.SearchFriendsCallback;
 import java.util.ArrayList;
 
 
-
 public class FriendEventFragment extends Fragment {
 
-
-    private TextView header_title;
-    private TextView no_msg;
-    private boolean auto = true;
-    private RecyclerView alert_list;
-    RecyclerView mRecyclerView;
-
-    private int page = 1;
-    private Player selectPlayerLists;
-    private SmartRefreshLayout refreshLayout;
-    private EditText search_bt;
 
     public FriendEventFragment() {
         // Required empty public constructor
@@ -48,63 +36,10 @@ public class FriendEventFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_friend_event, container, false);
-        mRecyclerView = (RecyclerView) view.findViewById(R.id.fast_scroller_recycler);
-        search_bt = (EditText) view.findViewById(R.id.search_bt);
-       // mRecyclerView.setIndexBarColor("#199899");
-        refreshLayout = (SmartRefreshLayout) view.findViewById(R.id.refreshLayout);
-        search_bt.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-
-            }
-
-            @Override
-            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-                Log.e("charSequence",""+charSequence+"");
-                if (!(charSequence+"").equalsIgnoreCase("")){
-                    refreshItems("filters", charSequence+"", "", -1, -1, -1, "", -1);
-                }else {
-                    refreshItems("default", "", "", -1, -1, -1, "", -1);
-                }
-
-            }
-
-            @Override
-            public void afterTextChanged(Editable editable) {
-
-            }
-        });
-        refreshLayout.autoRefresh();
-        refreshItems("default", "", "", -1, -1, -1, "", -1);
 
         return view;
 
     }
 
-    private void refreshItems(String type, String player_name, String gender,
-                              int country_id, int minage, int maxage, String rate, int sport_id) {
-        SearchFriendsAsyncTask recycleViewFindPlayer = new SearchFriendsAsyncTask(FriendEventFragment.this.getActivity(), type, player_name,
-                gender, country_id, minage, maxage, rate, sport_id, new SearchFriendsCallback() {
-            @Override
-            public void finishProcess(Player player) {
-                selectPlayerLists = player;
-                RecycleViewFriendsListEvent recycleViewFindPlayer = new RecycleViewFriendsListEvent(player, new PlayerListCallback() {
-                    @Override
-                    public void processFinish(ArrayList<Player.PlayerList> playerLists) {
-
-                    }
-                });
-                mRecyclerView.setAdapter(recycleViewFindPlayer);
-                onItemsLoadComplete();
-            }
-        });
-        recycleViewFindPlayer.execute();
-
-
-    }
-
-    private void onItemsLoadComplete() {
-        refreshLayout.finishRefresh();
-    }
 
 }
